80861fd899d5a6f614d740498f02c76f181287f0
shimoncohen <shimoncohen655@gmail.com>
2020-09-23 16:01:55 +0300

    feat(cors): add cors middleware

--- projectTemplates/js-server/src/server.js
+++ projectTemplates/js-server/src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const serverInstance = express();
+
+// Use cors middleware
+serverInstance.use(cors());
 
 serverInstance.get('/helloworld', async function(req, res) {
     return res.status(200).json({ hello: 'world' });
