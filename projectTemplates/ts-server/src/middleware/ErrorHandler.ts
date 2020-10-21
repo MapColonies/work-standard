d1c608d8b6889a8846280fd5f53b160eef179beb
Shahar s <cl_shaharshavit@rnd-hub.com>
2020-10-21 11:52:16 +0300

    feat(validation): added validation error handling

--- projectTemplates/ts-server/src/middleware/ErrorHandler.ts
+++ projectTemplates/ts-server/src/middleware/ErrorHandler.ts
@@ -2,6 +2,8 @@ import { MCLogger } from '@map-colonies/mc-logger';
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import { injectable } from 'tsyringe';
 import { StatusCodes } from 'http-status-codes';
+import { InputValidationError } from 'openapi-validator-middleware';
+import HttpStatus from 'http-status-codes';
 
 @injectable()
 export class ErrorHandler {
@@ -15,10 +17,14 @@ export class ErrorHandler {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       next: NextFunction
     ): void => {
-      this.logger.error(
-        `${req.method} request to ${req.originalUrl}  has failed with error: ${err.message}`
-      );
-      res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+      if (err instanceof InputValidationError) {
+        res.status(HttpStatus.BAD_REQUEST).json(err.errors);
+      } else {
+        this.logger.error(
+          `${req.method} request to ${req.originalUrl}  has failed with error: ${err.message}`
+        );
+        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+      }
     };
   }
 }
