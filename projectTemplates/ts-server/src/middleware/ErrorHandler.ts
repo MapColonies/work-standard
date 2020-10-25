de2c0cf210a2ce77aa6d13944c90c3b178b51c6c
Shahar s <cl_shaharshavit@rnd-hub.com>
2020-10-25 09:06:14 +0200

    chore(cleanup): removed duplicate import

--- projectTemplates/ts-server/src/middleware/ErrorHandler.ts
+++ projectTemplates/ts-server/src/middleware/ErrorHandler.ts
@@ -1,7 +1,6 @@
 import { MCLogger } from '@map-colonies/mc-logger';
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import { injectable } from 'tsyringe';
-import { StatusCodes } from 'http-status-codes';
 import { InputValidationError } from 'openapi-validator-middleware';
 import HttpStatus from 'http-status-codes';
 
@@ -23,7 +22,7 @@ export class ErrorHandler {
         this.logger.error(
           `${req.method} request to ${req.originalUrl}  has failed with error: ${err.message}`
         );
-        res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+        res.sendStatus(HttpStatus.INTERNAL_SERVER_ERROR);
       }
     };
   }
